8cd0fe337421000e88533c983121e1cdb82e74a0
Anh Vu Le Quy <lequyanh@fel.cvut.cz>
2016-04-19 01:22:01 +0200

    Auth call

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -2,38 +2,48 @@
 
 namespace App\Presenters;
 
+use GuzzleHttp;
 use Maknz\Slack\Message;
 use Nette;
 use App\Model;
+use Psr\Http\Message\MessageInterface;
 
 
 class HomepagePresenter extends BasePresenter
 {
+
+    private $CLIENT_ID = '34986258439.35402087824';
+    private $CLIENT_SECRET = '2f05735ef69278dc7eeff195591f85ce';
 
     public function renderDefault()
     {
         $this->template->anyVariable = 'any value';
     }
 
+    /**
+     * @param $contentArray Array (like JSON) of parameters to send
+     * @param $apiMethod string SlackApi method (oauth.access, channels.list)
+     *
+     * @return mixed JSON
+     */
+    public function sendApiRequest($contentArray, $apiMethod)
+    {
+        $client = new GuzzleHttp\Client(['base_uri' => 'https://slack.com/api/']);
+
+        $response = $client->request('GET', $apiMethod, ['query' => $contentArray]);
+
+        return $this->getJsonFromResponse($response);
+    }
+
     public function renderSlackAuth($code, $state)
     {
-        $clientId = '34986258439.35667661124';
+        $requestArray = array('client_id' => $this->CLIENT_ID,
+            'client_secret' => $this->CLIENT_SECRET,
+            'code' => $code);
 
-        $client = new \GuzzleHttp\Client();
-        $response =  $client->post('https://slack.com/api/oauth.access', [
-            'headers' => [
-                'content-type' => 'application/json',
-            ],
-            'body' => json_encode([
-                'client_id' => $clientId,
-                'client_secret' => '0557658ce0dea7b7683f4a5921de53b0',
-                'code' => $code,
-            ]),
-        ]);
+        $responseJSON = $this->sendApiRequest($requestArray, 'oauth.access');
 
-        echo $response->getBody()->getContents();
-
-
+        var_dump($responseJSON);
         exit(0);
     }
 
@@ -49,4 +59,8 @@ class HomepagePresenter extends BasePresenter
         $this->terminate();
     }
 
+    private function getJsonFromResponse(MessageInterface $response)
+    {
+        return Nette\Utils\Json::decode($response->getBody());
+    }
 }
