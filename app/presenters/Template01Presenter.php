6b27a9053ecd8ef9d4bc7582558e7a367e9d6093
kuzmamar <martin.kuzma90@gmail.com>
2016-04-18 21:04:35 +0200

    test form 01

--- app/presenters/Template01Presenter.php
+++ app/presenters/Template01Presenter.php
@@ -11,7 +11,16 @@ namespace App\Presenters;
 
 class Template01Presenter extends BasePresenter
 {
+  /** @var FormFactory @inject */
+	public $formFactory;
 
-
-
+  protected function createComponentForm()
+  {
+    $form = $this->formFactory->create();
+    $form->addText('test_text1', 'Test text 1:')
+         ->addText('test_text2', 'Test text 2:')
+         ->addText('test_text3', 'Test text 3:')
+         ->addSubmit('send', 'Send:');
+    return $form;
+  }
 }
