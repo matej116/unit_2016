8a0618606fff8e018d75808b85c969a90eca052a
Matěj Polák <matejpolak@seznam.cz>
2016-04-19 13:46:08 +0200

    unified template, live dashboard

--- app/presenters/DashboardPresenter.php
+++ app/presenters/DashboardPresenter.php
@@ -10,15 +10,13 @@ class DashboardPresenter extends BasePresenter
 	/** @var  DbMessageStorage @inject */
 	public $messageStorage;
 
-	public function renderDashboard()
+	public function renderDefault()
 	{
 		$this->template->dailyStats = $this->messageStorage->countDay();
 		$this->template->top10Messages = $this->messageStorage->topTenMessagesPerDay();
 		$this->template->top10People = $this->messageStorage->topTenPeoplePerDay();
 		$this->template->newMessages = $this->messageStorage->getMessages();
-		$this->redrawControl('dailyStats');
-		$this->redrawControl('top10Messages');
-		$this->redrawControl('top10People');
+		$this->redrawControl('dashboard');
 	}
 
 }
