5775e80615fd88d19e7c468b4bcf71b6fcaf545b
Ducan <Ducan>
2016-04-19 11:41:45 +0200

    dashboard

---
+++ app/presenters/DashboardPresenter.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ducan
+ * Date: 19. 4. 2016
+ * Time: 11:39
+ */
+
+namespace App\Presenters;
+
+
+class DashboardPresenter extends BasePresenter
+{
+
+    public function renderDashboard() {
+
+        
+
+    }
+
+}
